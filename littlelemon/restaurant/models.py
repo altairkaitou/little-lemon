6b41bb50a78532e275407980dd05d6352fe33555
Minh Bao <leminhgiabao@gmail.com>
2024-11-08 22:19:31 +0700

    Connect To MySQL DB and create Booking and Menu Model

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+
+
+# Booking Model:
+
+class Booking(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField()
+    BookingDate = models.DateTimeField()
+
+
+# Menu Model:
+
+class Menu(models.Model):
+    ID = models.AutoField(primary_key= True)
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.IntegerField()
+
+    def __str__(self):
+        return self.title
